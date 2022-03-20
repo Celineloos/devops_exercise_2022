f7fca85a87720ad39f424012603b96d4d4d38f0f
Celine <liefs_celine@hotmail.com>
2022-03-20 15:28:58 +0100

    fix cors

--- api/app.js
+++ api/app.js
@@ -21,6 +21,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// middleware for access
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
